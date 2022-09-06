77191cf0d20790f62d4f816e8c50758c58f91669
knebadze <nebo.nebadze98@gmail.com>
2022-09-06 09:53:37 +0400

    first commit

--- app/Models/Interviewer.php
+++ app/Models/Interviewer.php
@@ -8,13 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Interviewer extends Model
 {
     use HasFactory;
-    /**
-     * Get the user that owns the Interviewer
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function region(): BelongsTo
-    {
-        return $this->belongsTo(region::class, 'foreign_key', 'other_key');
-    }
+  
 }
--- app/Models/Region.php
+++ app/Models/Region.php
@@ -14,7 +14,16 @@ class Region extends Model
     public function user(){
         return $this->hasMany('App\Models\user');
     }
-    public function interviewer(){
-        return $this->hasOne('App\Models\interviewer');
+    // public function interviewer(){
+    //     return $this->hasOne('App\Models\interviewer');
+    // }
+    /**
+     * Get the user associated with the Region
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function interviewer(): HasOne
+    {
+        return $this->hasOne(interviewer::class, 'region_id');
     }
 }
